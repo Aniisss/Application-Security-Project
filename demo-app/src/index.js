aab1594633288a03e8aea6896e65de3b85f4d9a1
Aniisss <anisnsir74@gmail.com>
2026-01-11 15:03:59 +0100

    Adding the middleware that will serve the static files of the demo-app throught the bakckend , adding the .env file and updating the index.js to use the env variables instead of static variables to change them later for the prod env and udating the .gitignore to remove the .env and node_modules from the push to the repo

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+
+// 2. Use variables from .env (with fallbacks if they are missing)
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Middleware to parse JSON bodies (essential for API requests)
 app.use(express.json());
@@ -11,6 +19,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the Server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
